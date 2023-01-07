9eb74e7d4cab6c3f11556c17d31251a8ed9835c6
Sakina <sakina@Sakinas-MacBook-Air.local>
2023-01-07 17:04:07 -0500

    Add Spring Boot Server.

---
+++ server/shoppinglist/src/main/java/com/evaverma/shoppinglist/ShoppinglistApplication.java
@@ -0,0 +1,13 @@
+package com.evaverma.shoppinglist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppinglistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppinglistApplication.class, args);
+	}
+
+}
---
+++ server/shoppinglist/src/test/java/com/evaverma/shoppinglist/ShoppinglistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evaverma.shoppinglist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppinglistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
